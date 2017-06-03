90935b9c81c7c786188d476e82dc995c4bfe77d1
abk182 <abk1612@gmail.com>
2017-06-03 18:44:49 +0300

    Перенос уроков

---
+++ Lesson0/main.js
@@ -0,0 +1,21 @@
+var x= document.getElementById('numberX');
+var y= document.getElementById('numberY');
+
+document.getElementById('plus').addEventListener('click', function() {
+  console.log(+x.value + +y.value);
+  document.getElementById('answer').innerText = +x.value + +y.value;
+  //document.getElementById('answer').append(+x.value + +y.value);
+  //document.write(+x.value + +y.value);
+});
+
+document.getElementById('minus').addEventListener('click', function() {
+  document.getElementById('answer').innerText = +x.value - +y.value;
+});
+
+document.getElementById('division').addEventListener('click', function() {
+  document.getElementById('answer').innerText = +x.value / +y.value;
+});
+
+document.getElementById('multi').addEventListener('click', function() {
+  document.getElementById('answer').innerText = +x.value * +y.value;
+});
---
+++ Lesson1/main.js
@@ -0,0 +1,34 @@
+var x = 14;
+var y = 15;
+
+var name1 = "Apple";
+var name2 = "Pen";
+
+function nameFunc(x,y) {
+	if (y == undefined || x== undefined)	
+	{
+		document.write("Не указан один из аргументов");
+	} else {
+		document.write(x+y);
+	}
+}
+
+nameFunc();
+
+
+
+/*
+= - присваивание
+== - равенство
+!= - неравенство 
+< - меньше
+> - больше
+!< >= - неменьше
+!> <= - небольше
+
+Логические операторы
+! - не
+&& - и 
+|| - или
+
+*/
