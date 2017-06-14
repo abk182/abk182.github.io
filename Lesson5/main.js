3f4019fe0717048770db2856ae2d7928b8a82ab2
1Renat <rent1995@yandex.ru>
2017-06-14 19:50:09 +0300

    Lesson5

---
+++ Lesson5/main.js
@@ -0,0 +1,10 @@
+var container= document.querySelector('.root'); //document.getElementById();
+console.log(container);
+
+var jqueryContainer=$('#root');
+console.log(jqueryContainer);
+
+// $.ajax({
+// 	url:'',
+// 	type:'Get',
+// })
